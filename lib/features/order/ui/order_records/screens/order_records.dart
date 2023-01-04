ef0447040a3ce33100890c224ebde54f8b9d4c41
S A Saharukh <sasaharukh@gmail.com>
2023-01-05 01:09:49 +0600

    wip: order records page

--- lib/features/order/ui/order_records/screens/order_records.dart
+++ lib/features/order/ui/order_records/screens/order_records.dart
@@ -1,18 +1,26 @@
+import 'package:elvan/features/order/ui/components/order_records_app_bar.dart';
+import 'package:elvan/features/order/ui/order_records/notifier/order_records_notifier.dart';
+import 'package:elvan/shared/components/text/app_text_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import 'package:elvan/features/order/ui/recent_order/notifier/recent_order_notifier.dart';
-import 'package:elvan/features/order/ui/recent_order/screens/recent_order_grid.dart';
 
 class OrdersRecordsScreen extends HookConsumerWidget {
   const OrdersRecordsScreen({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final recentOrderNotifier = ref.watch(recentOrderNotifierProvider);
+    final orderRecordsNotifier = ref.watch(orderRecordsNotifierProvider);
 
-    return recentOrderNotifier.when(
+    return orderRecordsNotifier.when(
       loading: () => const Center(child: CircularProgressIndicator()),
-      data: (cartItems) => RecentOrdersGrid(cartItems: cartItems),
+      data: (orders) => Scaffold(
+        appBar: const OrderRecordsAppBar(),
+        body: ListView.builder(
+          itemCount: orders.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Text('Order: ${orders[index].id} ');
+          },
+        ),
+      ),
       error: (e, st) => Center(
         child: Text(e.toString()),
       ),
