991b046fdcd58ac58f6871e82dee9d92ab86874a
S A Saharukh <sasaharukh@gmail.com>
2023-02-10 23:19:02 +0600

    fav working

--- lib/features/favorite/provider/favorite_provider.dart
+++ lib/features/favorite/provider/favorite_provider.dart
@@ -1,19 +1,45 @@
+import 'dart:convert';
+
 import 'package:elvan_shared/domain_models/index.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final favoriteProvider =
     StateNotifierProvider<FavoriteProvider, List<FoodItem>>(
         (ref) => FavoriteProvider());
 
 class FavoriteProvider extends StateNotifier<List<FoodItem>> {
-  FavoriteProvider() : super([]);
+  FavoriteProvider() : super([]) {
+    loadFavorite();
+  }
 
   void addFavorite(FoodItem? foodItem) {
     if (foodItem == null) return;
 
     var temp = state;
     state = [...temp, foodItem];
+    saveFavorite();
+  }
+
+  //save to local storage
+  void saveFavorite() {
+    var list = state.map((e) {
+      return jsonEncode(e.toJson());
+    }).toList();
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.setStringList('favorite', list);
+    });
+  }
+
+  //load from local storage
+  void loadFavorite() {
+    SharedPreferences.getInstance().then((prefs) {
+      var list = prefs.getStringList('favorite');
+      if (list != null) {
+        state = list.map((e) => FoodItem.fromJson(jsonDecode(e))).toList();
+      }
+    });
   }
 
   void removeFavorite(FoodItem? foodItem) {
@@ -23,11 +49,12 @@ class FavoriteProvider extends StateNotifier<List<FoodItem>> {
 
     temp.removeWhere((element) => element.id == foodItem.id);
     state = [...temp];
+    saveFavorite();
   }
 
   bool isFavorite(FoodItem? foodItem) {
     if (foodItem == null) return false;
-
+    saveFavorite();
     return state.contains(foodItem);
   }
 }
