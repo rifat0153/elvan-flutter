c8ae2c0cee07d4afc166cc5e4aa900ec01424ac3
Mahbubur Rahman <rifat0153@gmail.com>
2023-03-04 10:39:30 +0000

    Fix: Recent order list is now refreshing after login. Currer user ID was not refreshing before due to watching the wrong Provider.

--- lib/features/order/ui/recent_order/notifier/recent_order_notifier.dart
+++ lib/features/order/ui/recent_order/notifier/recent_order_notifier.dart
@@ -1,41 +1,48 @@
-import 'package:elvan_shared/domain_models/index.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import 'package:elvan/core/ui_state/ui_state.dart';
+import 'package:elvan/core/logger/colored_print_log.dart';
 import 'package:elvan/features/auth/providers/auth_providers.dart';
-import 'package:elvan/features/cart/domain/models/cart_item/cart_item.dart';
 import 'package:elvan/features/order/domain/usecases/order_use_case.dart';
+import 'package:elvan_shared/domain_models/index.dart';
 
-final recentOrderNotifierProvider =
-    AsyncNotifierProvider<RecentOrderNotifier, List<CartItem>>(
-  () => RecentOrderNotifier(),
-);
+final recentOrderNotifierProvider = FutureProvider<List<CartItem>>((ref) async {
+  final orderUseCase = ref.read(orderUseCaseProvider);
+  final userId = ref.watch(currentUserIdProvider);
 
-class RecentOrderNotifier extends AsyncNotifier<List<CartItem>> {
-  late final OrderUseCase orderUseCase;
+  logInfo('getRecentOrders: userId: $userId');
 
-  @override
-  FutureOr<List<CartItem>> build() {
-    orderUseCase = ref.read(orderUseCaseProvider);
-
-    return getRecentOrders();
+  if (userId == null) {
+    throw Exception('User is not logged in');
   }
 
-  Future<List<CartItem>> getRecentOrders() async {
-    final userId = ref.read(currentUserIdProvider);
+  final result = await orderUseCase.getRecentOrders(userId, limit: 4);
+  final cartItems = result.expand((order) => order.items).toList();
 
-    if (userId == null) {
-      throw Exception('User is not logged in');
-    }
+  return cartItems;
+});
 
-    final result = await orderUseCase.getRecentOrders(userId, limit: 4);
 
-    final cartItems = <CartItem>[];
+//* same as above but using AsyncNotifierProvider
 
-    for (final order in result) {
-      cartItems.addAll(order.items);
-    }
+// final recentOrderNotifierProvider = AsyncNotifierProvider.autoDispose<RecentOrderNotifier, List<CartItem>>(
+//   () => RecentOrderNotifier(),
+// );
 
-    return cartItems;
-  }
-}
+// class RecentOrderNotifier extends AutoDisposeAsyncNotifier<List<CartItem>> {
+//   @override
+//   FutureOr<List<CartItem>> build() async {
+//     final orderUseCase = ref.read(orderUseCaseProvider);
+//     final userId = ref.watch(currentUserIdProvider);
+
+//     logInfo('getRecentOrders: userId: $userId');
+
+//     if (userId == null) {
+//       throw Exception('User is not logged in');
+//     }
+
+//     final result = await orderUseCase.getRecentOrders(userId, limit: 4);
+//     final cartItems = result.expand((order) => order.items).toList();
+
+//     return cartItems;
+//   }
+// }
