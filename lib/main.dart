d8e40cb81b40c6df72ab010d39ce4d5243883d7c
Mahbubur Rahman <rifat0153@gmail.com>
2023-04-01 12:00:03 +0100

    supabase dependency removed

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'package:elvan/app/app.dart';
-import 'package:elvan/core/constants/constants.dart';
 import 'package:elvan/core/logger/riverpod_state_logger.dart';
 
 import 'firebase_options.dart';
@@ -16,11 +14,6 @@ void main() async {
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  await Supabase.initialize(
-    url: Constants.url,
-    anonKey: Constants.anonKey,
   );
 
   runApp(
