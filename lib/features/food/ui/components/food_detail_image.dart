1e2825e6f14b0db3fb72794995a6fccc1a6526ad
rayhan13ads <rayhan13ads@gmail.com>
2023-04-14 15:57:26 +0600

    food details confilct issue done

--- lib/features/food/ui/components/food_detail_image.dart
+++ lib/features/food/ui/components/food_detail_image.dart
@@ -16,21 +16,3 @@ class FoodDetailImage extends StatelessWidget {
     );
   }
 }
-import 'package:elvan_shared/domain_models/index.dart';
-import 'package:flutter/material.dart';
-import 'package:elvan/shared/components/image/elvan_network_image.dart';
-
-class FoodDetailImage extends StatelessWidget {
-  const FoodDetailImage({super.key, required this.foodItem});
-
-  final FoodItem foodItem;
-
-  @override
-  Widget build(BuildContext context) {
-    return ElvanNetworkImage(
-      foodItem.imageUrl ?? '',
-      foodItem.id ?? '',
-      fit: BoxFit.cover,
-    );
-  }
-}
